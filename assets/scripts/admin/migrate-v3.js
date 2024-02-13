b865f38dcd5461bb0879a3488348ccee32f02d26
Marcin Pietrzak <marcin@iworks.pl>
2024-02-13 07:35:39 +0100

    prepare new version

--- assets/scripts/admin/migrate-v3.js
+++ assets/scripts/admin/migrate-v3.js
@@ -1,4 +1,4 @@
-/*! Omnibus — show the lowest price - v3.0.0.beta2
+/*! Omnibus — show the lowest price - v3.0.0.beta3
  * http://iworks.pl/
  * Copyright (c) 2024; * Licensed GPL-3.0 */
 (function($) {
