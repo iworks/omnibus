70eaa6cb741f68e20e09b02e7ff87fd89bd44c35
Marcin Pietrzak <marcin@iworks.pl>
2024-08-09 07:05:39 +0200

    prepare new version

--- assets/scripts/admin/woocommerce.js
+++ assets/scripts/admin/woocommerce.js
@@ -1,4 +1,4 @@
-/*! Omnibus — show the lowest price - v3.0.2
+/*! Omnibus — show the lowest price - v3.0.3
  * https://github.com/iworks/omnibus/
  * Copyright (c) 2024; * Licensed GPL-3.0 */
 (function($) {
