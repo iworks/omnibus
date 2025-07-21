712073909f91c4b7afe5f45d216c04e1909c6aff
Marcin Pietrzak <marcin@iworks.pl>
2025-07-21 09:50:11 +0200

    prepare new version

--- assets/scripts/admin/migrate-v3.js
+++ assets/scripts/admin/migrate-v3.js
@@ -1,6 +1,6 @@
-/*! Omnibus — show the lowest price - v3.0.3
+/*! Omnibus — show the lowest price - v3.0.4
  * https://github.com/iworks/omnibus/
- * Copyright (c) 2024; * Licensed GPL-3.0 */
+ * Copyright (c) 2025; * Licensed GPL-3.0 */
 (function($) {
     window.iworks_omnibus_migration_v3 = function() {
         $.ajax({
--- assets/scripts/admin/woocommerce.js
+++ assets/scripts/admin/woocommerce.js
@@ -1,6 +1,6 @@
-/*! Omnibus — show the lowest price - v3.0.3
+/*! Omnibus — show the lowest price - v3.0.4
  * https://github.com/iworks/omnibus/
- * Copyright (c) 2024; * Licensed GPL-3.0 */
+ * Copyright (c) 2025; * Licensed GPL-3.0 */
 (function($) {
     $(function() {
         $('input#_iwo_price_lowest_message_settings')
